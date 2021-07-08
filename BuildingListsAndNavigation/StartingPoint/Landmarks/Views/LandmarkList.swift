3ec654129e9e9ef0924d904d97f1de8e3b647650
Jean Claude Mbiya <mbiyajeanclaude@gmail.com>
2021-07-08 13:08:44 +0100

    building navigation Landmarks

---
+++ BuildingListsAndNavigation/StartingPoint/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by John Walter on 2021-07-08.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(
+                    destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
